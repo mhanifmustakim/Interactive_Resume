4eb5080d03da9f9dbd19667f1626564fdb7303e9
mhanifmustakim <mhanifmustakim@gmail.com>
2021-05-25 20:13:39 +0800

    First Prototype

---
+++ app.js
@@ -0,0 +1,21 @@
+const headImg = document.querySelector("#head-img");
+const name = document.querySelector("#name");
+const intro = document.querySelector("#intro");
+const mainContent = document.querySelector("#main-content");
+
+intro.style.height = `calc(100vh - ${headImg.clientHeight}px - ${name.clientHeight}px)`;
+mainContent.style.height = `calc(100vh - ${name.clientHeight}px)`;
+
+// Checks for scroll UP or DOWN
+let lastScrollTop = 0;
+
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+// window.addEventListener("scroll", function () { // or window.addEventListener("scroll"....
+//     var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+//     if (st > lastScrollTop) {
+//         console.log("Scrolled Down")
+//     } else {
+//         console.log("Scrolled Up")
+//     }
+//     lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+// }, false);
